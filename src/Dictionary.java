59a2c5e0b8d1d18ee64eaacc26c1f24846de7696
Alex Vasquez <caalevas@gmail.com>
2020-02-05 09:23:03 -0500

    Help

---
+++ src/Dictionary.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+	
+	public class Dictionary extends Application {
+	
+	public static void main(String[] args) {
+		
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Title_of_the_window");
+		button = new Button();
+		button.setText("Click me");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 300, 250);
+		PrimaryStage.setScene(scene);
+		PrimaryStage.show();
+	}
+	
+}
