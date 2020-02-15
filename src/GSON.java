4692ca92a1403d09ca8a21cb5fc675510c379c31
Alexander Vasquez <Acexander0@gmail.com>
2020-02-15 16:39:33 -0500

    Need to configure Word class to fit Json

--- src/GSON.java
+++ src/GSON.java
@@ -1,20 +1,25 @@
+//package reading_file;
+
+import java.io.*;
+import java.util.ArrayList;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
-import com.google.gson.JsonSyntaxException;  
+import com.google.gson.JsonSyntaxException; 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 
-public class GSON { 	
-   public static void main(String[] args) throws JsonSyntaxException, JsonIOException, FileNotFoundException {      
-      GsonBuilder builder = new GsonBuilder(); 
-      builder.setPrettyPrinting(); 
-      //JSONObject json = new JSONObject(jsonResult);
-      Gson gson = builder.create(); 
-     // String loudScreaming = json.getJSONObject("praise").getString("definition[1]");
-      Words word = gson.fromJson(new FileReader("../GSON/words.json"), Words.class); 
-      //System.out.println(loudScreaming);    
- 
-   } 
-} 
+public class GSON {
+public static void main(String [] args) throws JsonSyntaxException, JsonIOException, FileNotFoundException {
+	Words[] allWords = new Gson().fromJson(new FileReader("C:\\Users\\Acexa\\Desktop\\APCSA\\pset-10\\JSON\\words.json"), Words[].class);
+	System.out.println(allWords[3]);
+}
+
+}
--- src/Words.java
+++ src/Words.java
@@ -1,19 +1,27 @@
-import java.util.ArrayList; 
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException; 
+import com.fasterxml.jackson.annotation.JsonProperty;;
 
 public class Words {
+	
+	@JsonProperty("word")
 	public String word;
+	@JsonProperty("definitions")
 	ArrayList<String> definitions = new ArrayList<String>(); 
-	ArrayList<String> partsOfSpeech = new ArrayList<String>(); 
+	//ArrayList<String> partsOfSpeech = new ArrayList<String>(); 
 	ArrayList<String> synonyms = new ArrayList<String>();
 	ArrayList<String> antonyms = new ArrayList<String>();
 	
-	public Words(String word) {
-		this.word = word;
+	public Words(String word ) throws JsonSyntaxException, JsonIOException, FileNotFoundException {
+		Words[] allWords = new Gson().fromJson(new FileReader("C:\\Users\\Acexa\\Desktop\\APCSA\\pset-10\\JSON\\words.json"), Words[].class);
 	}
 	public void setDefinition(String definition, String partsOfSpeech) {
 		this.definitions.add(definition);
-		this.partsOfSpeech.add(partsOfSpeech);
+		//this.partsOfSpeech.add(partsOfSpeech);
 	}
 	public void addSynonym(String synonym) {
 		this.synonyms.add(synonym);
@@ -22,7 +30,7 @@ public class Words {
 		this.antonyms.add(antonym);
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//System.out.println(allWords[0]);
 
 	}
 
